587e158b9b614db30dec591f6a19805cdddae903
Jahan Rashidi <56563402+ImNotJahan@users.noreply.github.com>
2021-12-23 20:26:52 -0800

    Update quote-drawer.py

--- quote-drawer.py
+++ quote-drawer.py
@@ -1,5 +1,7 @@
 import requests
 from PIL import Image, ImageFont, ImageDraw
+import ctypes
+import os
 
 url = "http://www.forbes.com/forbesapi/thought/uri.json?enrich=true&query=1&relatedlimit=5"
 response = requests.get(url)
@@ -7,12 +9,14 @@ data = response.json()
 
 quote = data["thought"]["quote"]
 quote = quote.replace(',', ",\n")
-print(quote)
 
-BACKGROUND = Image.open("background.jpeg")
-FONT = ImageFont.truetype("CONSOLA.TTF", 12)
+BACKGROUND = Image.open("background.jpg")
+FONT = ImageFont.truetype("CONSOLA.TTF", 70)
 USABLE_BACKGROUND = ImageDraw.Draw(BACKGROUND)
 
-USABLE_BACKGROUND.text((15,15), quote, (0, 0, 0), font=FONT)
+WIDTH, HEIGHT = BACKGROUND.size
 
-BACKGROUND.save("changed_background.jpeg")
+USABLE_BACKGROUND.text((WIDTH / 2, HEIGHT / 2), quote, (255, 255, 255), font=FONT, anchor="mm")
+
+BACKGROUND.save("changed_background.jpg")
+ctypes.windll.user32.SystemParametersInfoW(20, 0, 'D:\\changed_background.jpg', 0)
